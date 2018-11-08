ecc841475bf8d640b57574294557087c5f533ab5
jfischlmayr <jan.fischlmayr@gmx.net>
2018-11-08 10:01:59 +0100

    5 tests work

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -18,10 +18,6 @@ bool is_square_ok (File file, Rank rank) {
   return file - 'a' < 8 && file - 'a' >= 0 && rank < 8 && rank >= 0;
 }
 
-int convert_file_to_int (File file) {
-  return file - 'a';
-}
-
 bool is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type) {
   return pc.color == color && pc.type == type;
 }
@@ -35,31 +31,45 @@ void init_chess_board (ChessBoard chess_board) {
 }
 
 struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank) {
-  if(file > 'h' || file < 'a' || rank > 8 || rank < 1 || chess_board[rank][file-'a'].is_occupied) {
+  if(file > 'h' || (file < 'a' && rank > 8) || rank < 1) {
     return 0;
   }
-  return &chess_board[rank][convert_file_to_int(file)];
+  return &chess_board[rank - 1][file - 'a'];
 }
 
 bool is_square_occupied (ChessBoard chess_board, File file, Rank rank) {
-  return chess_board[rank][convert_file_to_int(file)].is_occupied;
+  return chess_board[rank - 1][file - 'a'].is_occupied;
 }
 
 bool add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece) {
-  // if (!is_square_occupied() && is_square_ok) {
-  //   chess_board[rank][convert_file_to_int(file)]
-  // }
+   if (!is_square_occupied(chess_board, file, rank - 1) && is_square_ok(file, rank - 1)) {
+     chess_board[rank - 1][file - 'a'].is_occupied = true;
+     chess_board[rank - 1][file - 'a'].piece = piece;
+     return true;
+  }
   return false;
 }
 
-struct ChessPiece 	get_piece (ChessBoard chess_board, File file, Rank rank)
+struct ChessPiece get_piece (ChessBoard chess_board, File file, Rank rank)
 {
+  if (is_square_ok(file, rank) && chess_board[rank - 1][file - 'a'].is_occupied) {
+    return chess_board[rank - 1][file - 'a'].piece;
+  }
   struct ChessPiece p;
+  p.type = NoPiece;
   return p;
 }
 void 	setup_chess_board (ChessBoard chess_board)
 {
-  
+  chess_board[0][0].piece = Rook;
+  chess_board
+  chess_board[0][1].piece = Knight;
+  chess_board[0][2].piece = Bishop;
+  chess_board[0][3].piece = Queen;
+  chess_board[0][4].piece = King;
+  chess_board[0][5].piece = Bishop;
+  chess_board[0][6].piece = Knight;
+  chess_board[0][7].piece = Rook;
 }
 
 
